ffbf50761b9b41abceace0cbb7378117eb424ab5
Tulio Monte Azul <tuliomonteazul@gmail.com>
2015-05-18 11:48:26 -0300

    Add unit tests for services.

---
+++ test/factory.js
@@ -0,0 +1,7 @@
+module.exports = {
+	token: '123456778910abc',
+	lead: {
+		email: 'lead@example.com',
+		id: 92929
+	}
+};
---
+++ test/services.js
@@ -0,0 +1,107 @@
+/*global describe, it, before */
+'use strict';
+var assert = require('assert');
+var lib = require('../lib/rdstation');
+var client = require('../lib/rdstation/client.js');
+var nock = require('nock');
+var factory = require('./factory.js');
+
+describe('Services', function () {
+	describe('when a deal is set to won', function () {
+		before(function () {
+			nock(client.baseURL)
+				.post('/api/1.2/services/' + factory.token + '/generic', {
+					status: 'won',
+					value: 999,
+					email: factory.lead.email
+				})
+				.reply(200);
+		});
+
+		it('should call the API with correct parameters', function (done) {
+			var services = new lib.Services(factory.token);
+			services.dealWon(999, factory.lead.email)
+				.then(function () {
+					done();
+				})
+				.catch(done);
+		});
+	});
+	describe('when a deal is set to lost', function () {
+		before(function () {
+			nock(client.baseURL)
+				.post('/api/1.2/services/' + factory.token + '/generic', {
+					status: 'lost',
+					lost_reason: 'market price is lower',
+					email: factory.lead.email
+				})
+				.reply(200);
+		});
+
+		it('should call the API with correct parameters', function (done) {
+			var services = new lib.Services(factory.token);
+			services.dealLost('market price is lower', factory.lead.email)
+				.then(function () {
+					done();
+				})
+				.catch(done);
+		});
+
+	});
+		describe('using the lead id instead of email', function () {
+			before(function () {
+				nock(client.baseURL)
+					.post('/api/1.2/services/' + factory.token + '/generic', {
+						status: 'lost',
+						lost_reason: 'market price is lower',
+						lead_id: factory.lead.id
+					})
+					.reply(200);
+			});
+
+			it('should call the API with correct parameters', function (done) {
+				var services = new lib.Services(factory.token);
+				services.dealLost('market price is lower', null, factory.lead.id)
+					.then(function () {
+						done();
+					})
+					.catch(done);
+			});
+		});
+
+});
+
+
+
+
+
+/*
+
+'use strict';
+var assert = require('assert');
+var rdstationNodeClient = require('../');
+var client = require('../lib/rdstation/client.js');
+var nock = require('nock');
+var factory = require('./factory.js')
+
+describe('Services', function () {
+	describe('when a deal is set to won', function () {
+		before(function () {
+			nock(client.baseURL)
+				.post('/conversions', {
+					'token_rdstation': factory.token,
+					'identificador': ''
+				})
+				.reply(201, {
+					ok: true,
+					id: '123ABC',
+					rev: '946B7D1C'
+				});
+		});
+
+	});
+	it('must have at least one test', function () {
+		assert(rdstationNodeClient.Services != null);
+	});
+});
+ */
