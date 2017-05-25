3c1c4d2ba3cba3e52de3b46cc624fb2c99ec1e2d
Yuri Levenhagen <yuri.levenhagem@candeias.tur.br>
2017-05-25 18:49:55 -0300

    An empty reporterOutput attribute allows grunt to work with jshint-stylish as reporter without generating extra files on the folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,8 @@ module.exports = function (grunt) {
     jshint: {
       options: {
         jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
+        reporter: require('jshint-stylish'),
+        reporterOutput: ''
       },
       gruntfile: {
         src: ['Gruntfile.js']
